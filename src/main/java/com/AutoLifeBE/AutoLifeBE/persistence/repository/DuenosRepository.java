264b6f95159963a0c9741a251278ad278216ecb6
Luis Andres Gonzalez Corzo <luisangoco26@gmail.com>
2024-04-15 01:23:08 -0500

    fix query errors

--- src/main/java/com/AutoLifeBE/AutoLifeBE/persistence/repository/DuenosRepository.java
+++ src/main/java/com/AutoLifeBE/AutoLifeBE/persistence/repository/DuenosRepository.java
@@ -5,16 +5,17 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  *
  * @author Luis Andres Gonzalez Corzo
  */
 public interface DuenosRepository extends JpaRepository<Duenos, Long>{
-    @Query(value = "SELECT d FROM Duenos d WHERE d.usuario = ?", nativeQuery = true)
-    Optional<List<Duenos>> findByIdUsuarios(String usuarios);
+    @Query(value = "SELECT * FROM duenos WHERE usuario = :usuarios", nativeQuery = true)
+    Optional<List<Duenos>> findByIdUsuarios(@Param("usuarios") String usuarios);
     
-    @Query(value = "SELECT d FROM Duenos d WHERE d.vehiculo = ?", nativeQuery = true)
-    Optional<List<Duenos>> findByIdVehiculos(String vehiculos);
+    @Query(value = "SELECT * FROM duenos WHERE vehiculo = :vehiculos", nativeQuery = true)
+    Optional<List<Duenos>> findByIdVehiculos(@Param("vehiculos") String vehiculos);
     
 }
