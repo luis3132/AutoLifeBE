a60f80f4d16bb9c3a59c1615fb0d4b996f5203ca
Luis Andres Gonzalez Corzo <luisangoco26@gmail.com>
2024-05-18 18:15:36 -0500

    add generated id value to Duenos class

--- src/main/java/com/AutoLifeBE/AutoLifeBE/persistence/entity/Duenos.java
+++ src/main/java/com/AutoLifeBE/AutoLifeBE/persistence/entity/Duenos.java
@@ -2,6 +2,8 @@ package com.AutoLifeBE.AutoLifeBE.persistence.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.Date;
@@ -20,6 +22,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "duenos")
 public class Duenos {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private String usuario;
