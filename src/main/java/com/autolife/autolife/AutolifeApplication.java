15d8551dc1773501d84fbd96203dcfd2ba563724
luis3132 <luisangoco26@gmail.com>
2024-10-29 10:57:10 -0500

    init project

---
+++ src/main/java/com/autolife/autolife/AutolifeApplication.java
@@ -0,0 +1,13 @@
+package com.autolife.autolife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutolifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autolife/autolife/AutolifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autolife.autolife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutolifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
